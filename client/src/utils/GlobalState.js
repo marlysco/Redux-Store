f073eb2275612c751fef5a16271bd176b29dc4e9
marlysco <marlysco94@gmail.com>
2021-08-22 19:58:09 -0400

    Updating code at GlobalSate and reducers, adding store.js

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,10 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React from "react";
+import { Provider } from 'react-redux';
+import store from './store';
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
-
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
-
-  return <Provider value={[state, dispatch]} {...props} />;
+const StoreProvider = ({...props }) => {
+  return <Provider store={store} {...props} />;
 };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+export { StoreProvider };
 
-export { StoreProvider, useStoreContext };
